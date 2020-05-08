2d01b7081d5784a9368ad52166ea0f37ead6d70d
Sehun Jeong <jjshh9095@gmail.com>
2020-05-09 06:34:44 +0900

    cors

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 // API Key 값 설정
 axios.defaults.headers['APIKey'] = process.env.VUE_APP_API_KEY;
+axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers['Content-Type'] = 'text/html';
 
 // 국내 환자 조회
 export function fetchKoreaData(locale) {
